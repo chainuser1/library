42a550bf412457bfcce870c56d872183fbb34562
Jayson R. Ramos <lothlorien654@gmail.com>
2016-02-03 17:02:54 -0800

    Add Register Page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,20 @@ class UsersController < ApplicationController
     @user=User.new
   end
   def register
+     @user=User.new(user_params)
 
+       respond_to do |format|
+         if @user.save
+           format.html {redirect_to 'welcome/index', notice: 'You may sign in now.'}
+         else
+           format.html {redirect_to 'new'}
+           format.json{@user}
+         end
+       end
+
+  end
+  private
+  def user_params
+    params.require(:user).permit(:email,:password)
   end
 end
