16502ac083e4a8cf30df6d60451223d520cfea89
Jayson R. Ramos <lothlorien654@gmail.com>
2016-02-04 18:24:21 -0800

    Make Per-page Titling Responsive

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   REGEX_EMAIL_PATTERN=/([a-z0-9_]+|[a-z0-9_]+\.[a-z0-9_]+)@(([a-z0-9]|[a-z0-9]+\.[a-z0-9]+)+\.([a-z]{2,4}))/i
   validates :email ,presence:true,
-                   format: {with: REGEX_EMAIL_PATTERN, message: ' is invalid!'}
+                   format: {with: REGEX_EMAIL_PATTERN, message: ' is invalid!'},
+                   uniqueness: true
 
   has_secure_password
   validates :password, presence:true,
