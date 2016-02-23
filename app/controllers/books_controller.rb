b17625f7eb2a7089036bceca87d24234cad39d53
Jayson R. Ramos <lothlorien654@gmail.com>
2016-02-23 08:55:13 -0800

    Modify Books Controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -115,6 +115,6 @@ class BooksController < ApplicationController
   end
   def book_params
     params.require(:book).permit :isbn,:title,:author, :category,:publisher,
-                         :copyright,:description
+                                 :copyright,:description
   end
 end
