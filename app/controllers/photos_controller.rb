fe95a25b63b5bd35b146a7842c25aab3f3b007c9
Jayson R. Ramos <lothlorien654@gmail.com>
2016-03-29 11:40:11 -0700

    Create PhotosController

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,13 +1,25 @@
 class PhotosController < ApplicationController
   def index
+    @photos=current_user.photos.all
   end
-
   def new
+    @photo=Photo.new
   end
-
   def create
+    @photo=Photo.new(photo_params)
+    if @photo.save
+      redirect_to photos_path, notice: "The #{@photo.name} has been uploaded"
+    else
+      render 'new'
+    end
   end
-
   def destroy
+    @photo=Photo.find(param[:id])
+    @photo.destroy
+    redirect_to photos_path, notice: "The photo #{@photo.name} has been deleted"
+  end
+  private
+  def photo_params
+    params.require(:photo).permit(:user_username,:name,:attachment)
   end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,4 @@
 class Photo < ActiveRecord::Base
+  mount_uploader :attachment, AttachmentUploader #Tells rails to use this uploader for this model
   belongs_to :user,foreign_key: :user_username, primary_key: 'username', dependent: :destroy
 end
